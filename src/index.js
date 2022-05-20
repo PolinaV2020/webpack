ec561a88f1f013bb54ad293aef75ac2bce26566f
polinav2020 <polinapiksi2019@mail.ru>
2022-05-20 22:41:57 +0300

    add: resolve, optimization, devserver

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import Post from "./Post";
+import * as $ from "jquery";
+import Post from "@models/Post";
 import json from "./assets/json";
+import xml from "./assets/data.xml";
+import csv from "./assets/data.csv";
 import WebpackLogo from "./assets/webpack-logo.png";
 import "./styles/styles.css";
 
 const post = new Post("1st post", WebpackLogo);
+$("pre").html(post.toString());
 console.log(post);
 console.log("json", json);
+console.log("xml", xml);
+console.log("csv", csv);
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,25 @@ module.exports = {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  resolve: {
+    // Если несколько файлов имеют одно и то же имя, но разные расширения,
+    //  webpack разрешит файл с расширением, указанным первым в массиве,
+    //  и пропустит остальные.
+    //  Позволяет пользователям не указывать расширение при импорте.
+    extensions: [".js", ".json"],
+    alias: {
+      "@models": path.resolve(__dirname, "src/models"),
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all" // Разделение bundles на 2 файла: с кодом разработчика и со сторонним кодом (кодом jquery, например)
+    }
+  },
+  devServer: {
+    port: 3000
+  },
   plugins: [
     // Plugin для автозамены bundles при внесении изменений в файлах
     new HTMLWebpackPlugin({
@@ -29,7 +48,20 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      },
+      {
+        test: /\.csv$/,
+        use: ["csv-loader"]
+      },
+      // Для картинок и шрифтов вместо loaders прописывается type
+      {
         test: /\.(png|jpg|svg|gif)$/,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
         type: "asset/resource"
       }
     ]

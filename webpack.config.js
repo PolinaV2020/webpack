0cd627b03b7dd198570d4357a357d1034c03011c
polinav2020 <polinapiksi2019@mail.ru>
2022-05-30 15:32:57 +0400

    add: optimization plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-// const isDev = process.env.NODE_ENV === "development";
-// console.log("isDev", isDev);
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all" // Разделение bundles на 2 файла: с кодом разработчика и со сторонним кодом (кодом jquery, например)
+    }
+  };
+
+  if (isProd) {
+    config.minimizer = [new CssMinimizerPlugin(), new TerserPlugin()]; // Минифицирует файлы CSS в проде
+  }
+
+  return config;
+};
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -28,13 +45,10 @@ module.exports = {
       "@": path.resolve(__dirname, "src")
     }
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all" // Разделение bundles на 2 файла: с кодом разработчика и со сторонним кодом (кодом jquery, например)
-    }
-  },
+  optimization: optimization(),
   devServer: {
-    port: 3000
+    port: 3000,
+    hot: isDev // Заменяет, добавляет или удаляет модули во время работы приложения без полной перезагрузки. Это может значительно ускорить разработку
   },
   plugins: [
     // Plugin для автозамены bundles при внесении изменений в файлах
@@ -51,14 +65,17 @@ module.exports = {
           to: path.resolve(__dirname, "dist")
         }
       ]
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].bundle.css"
+    }) // Извлекает CSS в отдельные файлы. Он создает файл CSS для каждого файла JS, который содержит CSS.
   ],
   module: {
     // Loaders позволяют делать импорты файлов форматов не JS и JSON
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.xml$/,
